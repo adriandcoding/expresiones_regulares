87aefe6593e357e74c083613bb04271c38d4ecb8
adriandcoding <theamr91@gmail.com>
2024-06-14 15:13:16 +0200

    cambios en index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,11 @@ import { validateIBAN, extractBankInfo } from "./validacion";
 
 const searchBtn = document.getElementById("validate");
 const ibanInput = document.getElementById("iban") as HTMLInputElement;
-const resultDiv = document.getElementById("result");
+const resultDiv = document.getElementById("resultado");
 
 if (searchBtn && ibanInput && resultDiv) {
   searchBtn.addEventListener("click", () => {
-    const iban = ibanInput.value.trim();
-
-    if (!iban) {
-      resultDiv.innerHTML = "<p>Por favor, introduce un IBAN.</p>";
-      return;
-    }
-
-    if (!validateIBAN(iban)) {
-      resultDiv.innerHTML = "<p>El IBAN introducido no es válido.</p>";
-      return;
-    }
-
-    const bankInfo = extractBankInfo(iban);
-
-    if (bankInfo) {
-      resultDiv.innerHTML = `
-                <p><strong>Banco:</strong> ${bankInfo.bankName}</p>
-                <p><strong>Oficina:</strong> ${bankInfo.office}</p>
-                <p><strong>Dígito de control:</strong> ${bankInfo.controlDigit}</p>
-                <p><strong>Número de cuenta:</strong> ${bankInfo.accountNumber}</p>
-            `;
-    } else {
-      resultDiv.innerHTML = "<p>No se pudo obtener información del banco.</p>";
-    }
+    console.log("hola");
+    resultDiv.innerHTML = "hola";
   });
 }
