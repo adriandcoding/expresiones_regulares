0967b9c2f657d163c937a79e119093e7e388f195
adriandcoding <theamr91@gmail.com>
2024-06-17 13:44:49 +0200

    creando estilos

--- APARTADO-B/src/main.ts
+++ APARTADO-B/src/main.ts
@@ -1,3 +1,9 @@
-import "./style.css";
-
-console.log("Hello Typescript!");
+function extractImageLinks(): void {
+  const htmlInput = (
+    document.getElementById("htmlInput") instanceof HTMLTextAreaElement
+  ).valueOf;
+  const imageLinksContainer =
+    document.getElementById("imageLinks") instanceof HTMLDivElement;
+  const imageGridContainer =
+    document.getElementById("imageGrid") instanceof HTMLDivElement;
+}
