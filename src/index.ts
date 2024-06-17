4e5a224e4fa8d0836069d672918b7e69b74e22a6
adriandcoding <theamr91@gmail.com>
2024-06-17 11:15:22 +0200

    cambios en index ts y html

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { validateIBAN, extractBankInfo } from "./validacion";
 
-const searchBtn = document.getElementById("validate");
-const ibanInput = document.getElementById("iban") as HTMLInputElement;
-const resultDiv = document.getElementById("resultado");
+const searchBtn = document.getElementById(
+  "validateButton"
+) as HTMLButtonElement | null;
+const ibanInput = document.getElementById("iban") as HTMLInputElement | null;
+const resultDiv = document.getElementById("resultado") as HTMLDivElement | null;
 
 if (searchBtn && ibanInput && resultDiv) {
   searchBtn.addEventListener("click", () => {
@@ -22,11 +24,11 @@ if (searchBtn && ibanInput && resultDiv) {
 
     if (bankInfo) {
       resultDiv.innerHTML = `
-                <p><strong>Banco:</strong> ${bankInfo.bankName}</p>
-                <p><strong>Oficina:</strong> ${bankInfo.office}</p>
-                <p><strong>Dígito de control:</strong> ${bankInfo.controlDigit}</p>
-                <p><strong>Número de cuenta:</strong> ${bankInfo.accountNumber}</p>
-            `;
+        <p><strong>Banco:</strong> ${bankInfo.bankName}</p>
+        <p><strong>Oficina:</strong> ${bankInfo.office}</p>
+        <p><strong>Dígito de control:</strong> ${bankInfo.controlDigit}</p>
+        <p><strong>Número de cuenta:</strong> ${bankInfo.accountNumber}</p>
+      `;
     } else {
       resultDiv.innerHTML = "<p>No se pudo obtener información del banco.</p>";
     }
